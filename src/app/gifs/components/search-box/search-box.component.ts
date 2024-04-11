c66158b182cf8ede5f5863e5c5fce58fed8ca84f
Juan <gurquiza8@gmail.com>
2024-04-10 23:30:31 -0500

    implementing viewchild for localreference

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,17 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
-    selector: 'gifs-search-box',
-    template: `
-        <h5>Buscar: </h5>
-        <input
-            type="text"
-            class="form-control"
-            placeholder="Buscar Gifs..."
-        />
-    `
+  selector: 'gifs-search-box',
+  template: `
+    <h5>Buscar: </h5>
+    <input
+      type="text"
+      class="form-control"
+      placeholder="Buscar Gifs..."
+      (keyup.enter)="searchTag()"
+      #txtTagInput
+    />
+  `
 })
 export class SearchBoxComponent {
+
+  @ViewChild('txtTagInput')
+  public tagInput!: ElementRef<HTMLInputElement>;
+
+  searchTag() {
+    const newTag = this.tagInput.nativeElement.value;
+    console.log({ newTag });
+  }
 
 }
 
