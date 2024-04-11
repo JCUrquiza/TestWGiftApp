a894a83d14fc2e683d7abcdca0ecd3c556de6759
Juan <gurquiza8@gmail.com>
2024-04-11 12:21:52 -0500

    Making a request to consume gifs

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Injectable({ providedIn: 'root' })
 export class GifsService {
 
   private _tagsHistory: string[] = [];
+  private apiKey: string = 'umXO25VzbLQZSu448xHAdqiF34HqO7uh';
+  private serviceURL: string = 'https://api.giphy.com/v1/gifs';
 
-  constructor() {}
+  constructor( private http: HttpClient ) {}
 
   get tagsHistory() {
     return [...this._tagsHistory]
@@ -27,6 +30,17 @@ export class GifsService {
   searchTag( tag: string ):void {
     if ( tag.length === 0 ) return;
     this.organizeHistory(tag);
+
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('limit', '10')
+      .set('q', tag)
+
+    this.http.get(`${ this.serviceURL }/search`, { params: params })
+      .subscribe( data => {
+        console.log(data);
+      });
+
   }
 
 }
