ac386c475283f065be1bf1552e1beb7ad20b8b0b
Juan <gurquiza8@gmail.com>
2024-04-10 19:12:09 -0500

    Make a card component

---
+++ src/app/gifs/components/cart-list/cart-list.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gifs-cart-list',
+  templateUrl: './cart-list.component.html',
+})
+export class CartListComponent {
+
+}
--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home/home-page.component';
 import { SearchBoxComponent } from './components/search-box/search-box.component';
+import { CartListComponent } from './components/cart-list/cart-list.component';
 
 
 @NgModule({
   declarations: [
     HomePageComponent,
-    SearchBoxComponent
+    SearchBoxComponent,
+    CartListComponent
   ],
   imports: [
     CommonModule
