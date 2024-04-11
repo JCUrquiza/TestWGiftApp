ea10f303e9be2861c79ad090cf80297e18a45097
Juan <gurquiza8@gmail.com>
2024-04-11 18:46:12 -0500

    Load the first word to show gif

--- src/app/gifs/components/cart-list/cart-list.component.ts
+++ src/app/gifs/components/cart-list/cart-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Gif } from '../../interfaces/gifs.interfaces';
+import { GifsService } from '../../services/gifs.service';
 
 @Component({
   selector: 'gifs-cart-list',
@@ -9,5 +10,18 @@ export class CartListComponent {
 
   @Input()
   public gifs: Gif[] = [];
+
+  public tag: string = '';
+
+  constructor( public gifsService: GifsService ) {
+    if (JSON.parse(localStorage.getItem('history')!)) {
+      this.tag = JSON.parse(localStorage.getItem('history')!)[0];
+      this.showFirstGifOfHostory(this.tag);
+    }
+  }
+
+  showFirstGifOfHostory(tag: string) {
+    this.gifsService.searchTag(tag);
+  }
 
 }
