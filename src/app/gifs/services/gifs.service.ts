afe6d2ad7da51eb94db8da9176aa30c3ab537e79
Juan <gurquiza8@gmail.com>
2024-04-11 18:06:17 -0500

    Fix a problem with array on sidebar

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -7,10 +7,12 @@ export class GifsService {
 
   public gifsList: Gif[] = [];
   private _tagsHistory: string[] = [];
-  private apiKey: string = 'umXO25VzbLQZSu448xHAdqiF34HqO7uh';
+  private apiKey: string = 'iWQSlaDJLDehsV2OEbH2WVNDN3QyNq9Y';
   private serviceURL: string = 'https://api.giphy.com/v1/gifs';
 
-  constructor( private http: HttpClient ) {}
+  constructor( private http: HttpClient ) {
+    this.loadLocalStorage();
+  }
 
   get tagsHistory() {
     return [...this._tagsHistory]
@@ -22,11 +24,23 @@ export class GifsService {
     // Si el tag ya está en el arreglo, entonces eliminarlo
     if ( this._tagsHistory.includes(tag) ) {
       this._tagsHistory = this._tagsHistory.filter((oldTag) => oldTag !== tag);
+    } else {
+      if ( this._tagsHistory.length >= 10 ) return;
     }
     // Para insertar el tag al inicio del arreglo
     this._tagsHistory.unshift( tag );
     // Y limitar el arreglo a 10 elementos
-    this._tagsHistory = this._tagsHistory.splice(1, 10);
+    // this._tagsHistory = this.tagsHistory.splice(1, 10);
+    this.saveLocalStorage();
+  }
+
+  private saveLocalStorage(): void {
+    localStorage.setItem('history', JSON.stringify(this._tagsHistory));
+  }
+
+  private loadLocalStorage(): void {
+    if ( !localStorage.getItem('history') ) return;
+    this._tagsHistory = JSON.parse(localStorage.getItem('history')!);
   }
 
   searchTag( tag: string ):void {
@@ -40,6 +54,7 @@ export class GifsService {
 
     this.http.get<SearchResponse>(`${ this.serviceURL }/search`, { params: params })
       .subscribe( respuesta => {
+        console.log(respuesta);
         this.gifsList = respuesta.data;
       });
 
