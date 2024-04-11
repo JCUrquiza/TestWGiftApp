a4e04284eae6c27e6cccc0c673e16c8987de55b2
Juan <gurquiza8@gmail.com>
2024-04-11 11:35:52 -0500

    Validations for save tags in array from search

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -11,8 +11,22 @@ export class GifsService {
     return [...this._tagsHistory]
   }
 
-  searchTag( tag: string ):void {
+  private organizeHistory(tag: string) {
+    // El tag se pasa a miníscula
+    tag = tag.toLowerCase();
+    // Si el tag ya está en el arreglo, entonces eliminarlo
+    if ( this._tagsHistory.includes(tag) ) {
+      this._tagsHistory = this._tagsHistory.filter((oldTag) => oldTag !== tag);
+    }
+    // Para insertar el tag al inicio del arreglo
     this._tagsHistory.unshift( tag );
+    // Y limitar el arreglo a 10 elementos
+    this._tagsHistory = this._tagsHistory.splice(1, 10);
+  }
+
+  searchTag( tag: string ):void {
+    if ( tag.length === 0 ) return;
+    this.organizeHistory(tag);
   }
 
 }
