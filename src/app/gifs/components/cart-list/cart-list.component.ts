968c6a42a80926822dafaa1d45d93b9258d545ec
Juan <gurquiza8@gmail.com>
2024-04-11 13:09:28 -0500

    Showing gifs in screen

--- src/app/gifs/components/cart-list/cart-list.component.ts
+++ src/app/gifs/components/cart-list/cart-list.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Gif } from '../../interfaces/gifs.interfaces';
 
 @Component({
   selector: 'gifs-cart-list',
   templateUrl: './cart-list.component.html',
 })
 export class CartListComponent {
+
+  @Input()
+  public gifs: Gif[] = [];
 
 }
