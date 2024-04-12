9704358932094d35fb4935ff49130f5447f925d0
Juan <gurquiza8@gmail.com>
2024-04-12 12:59:06 -0500

    Thinking abour components... for image

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home/home-page.component';
 import { SearchBoxComponent } from './components/search-box/search-box.component';
 import { CartListComponent } from './components/cart-list/cart-list.component';
+import { GifsCardComponent } from './components/gifs-card/gifs-card.component';
 
 
 @NgModule({
   declarations: [
     HomePageComponent,
     SearchBoxComponent,
-    CartListComponent
+    CartListComponent,
+    GifsCardComponent
   ],
   imports: [
     CommonModule
